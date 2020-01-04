76cf37ed56ff0a8bf6386e805ebee33cc342ea6d
zhairf <zhairf@tedu.cn>
2020-01-04 15:47:18 +0800

    First Test Git

---
+++ src/main/java/cn/tedu/git/Apple.java
@@ -0,0 +1,10 @@
+package cn.tedu.git;
+
+public class Apple {
+
+	public static void main(String[] args) {
+		System.out.println("Apple");
+
+	}
+
+}
---
+++ src/test/java/cn/tedu/TestApple.java
@@ -0,0 +1,10 @@
+package cn.tedu;
+
+public class TestApple {
+
+	public static void main(String[] args) {
+		System.out.println("TestApple");
+
+	}
+
+}
